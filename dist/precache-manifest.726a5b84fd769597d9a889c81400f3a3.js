7f6c7d2930acfb37c0acc127e5cf6da52999f818
Matthue <M@SnowyRange.dev>
2021-11-21 15:31:40 -0700

    adding back build folder since it seems to be needed

---
+++ dist/precache-manifest.726a5b84fd769597d9a889c81400f3a3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a948a304faf0e0862c4",
+    "url": "/socket-communicator/css/app.6027062f.css"
+  },
+  {
+    "revision": "9119cdc082d5b4e3fbb7",
+    "url": "/socket-communicator/css/chunk-vendors.18b04a77.css"
+  },
+  {
+    "revision": "b5f9b7953c04b8fea292663377f63b5c",
+    "url": "/socket-communicator/img/logo.b5f9b795.svg"
+  },
+  {
+    "revision": "9ef0bfdf64c387afc8d54810fae73fc9",
+    "url": "/socket-communicator/img/pattern.9ef0bfdf.svg"
+  },
+  {
+    "revision": "5d1ded608c0f24771512234d434a8250",
+    "url": "/socket-communicator/index.html"
+  },
+  {
+    "revision": "5a948a304faf0e0862c4",
+    "url": "/socket-communicator/js/app.0025dc1e.js"
+  },
+  {
+    "revision": "9119cdc082d5b4e3fbb7",
+    "url": "/socket-communicator/js/chunk-vendors.8601d24b.js"
+  },
+  {
+    "revision": "23204bc6e121ce770b8d93531ba0eb82",
+    "url": "/socket-communicator/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/socket-communicator/robots.txt"
+  }
+]);
