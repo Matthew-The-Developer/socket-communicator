fa4a342a3f2e38fd69cdbdc88341ef118a87076d
Matthue <M@SnowyRange.dev>
2021-11-21 15:25:24 -0700

    adding build subtree

---
+++ dist/precache-manifest.3a4503807dbc2e90bafc61003244e237.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f51fb02bc80194b60208",
+    "url": "/css/app.6027062f.css"
+  },
+  {
+    "revision": "9119cdc082d5b4e3fbb7",
+    "url": "/css/chunk-vendors.18b04a77.css"
+  },
+  {
+    "revision": "b5f9b7953c04b8fea292663377f63b5c",
+    "url": "/img/logo.b5f9b795.svg"
+  },
+  {
+    "revision": "9ef0bfdf64c387afc8d54810fae73fc9",
+    "url": "/img/pattern.9ef0bfdf.svg"
+  },
+  {
+    "revision": "5d2603f695b38d248c99fb5e25a9c291",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f51fb02bc80194b60208",
+    "url": "/js/app.6a8ff31a.js"
+  },
+  {
+    "revision": "9119cdc082d5b4e3fbb7",
+    "url": "/js/chunk-vendors.8601d24b.js"
+  },
+  {
+    "revision": "23204bc6e121ce770b8d93531ba0eb82",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
